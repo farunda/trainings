5527495fca59573b548b51cb4d1f9ae6f07f0b7f
Paul Farunda <farunda@yandex-team.ru>
2017-11-23 11:27:54 +0300

    Fixed spaces indentation

--- for_try.py
+++ for_try.py
@@ -6,7 +6,7 @@
 
 ten_digits_list = [3, 14, 15, 926, 535, 89, 79, 32, 38, 46]
 for digit in ten_digits_list:
-	print(digit + 1)
+    print(digit + 1)
 
 '''
 Цикл
@@ -16,7 +16,7 @@ for digit in ten_digits_list:
 
 string_1 = input("Please, enter some text here: ")
 for character in string_1:
-	print(character)
+    print(character)
 
 '''
 Оценки
@@ -26,32 +26,32 @@ for character in string_1:
 '''
 
 classes_list = [{'school_class': '4a', 'scores': [3,4,4,5,2]},
-				{'school_class': '8b', 'scores': [5,4,4,5,4]},
-				{'school_class': '6c', 'scores': [2,4,3,5,5]},
-				{'school_class': '5a', 'scores': [4,4,4,4,3]},
-				{'school_class': '7b', 'scores': [2,2,3,5,4]},
-			   ]
+                {'school_class': '8b', 'scores': [5,4,4,5,4]},
+                {'school_class': '6c', 'scores': [2,4,3,5,5]},
+                {'school_class': '5a', 'scores': [4,4,4,4,3]},
+                {'school_class': '7b', 'scores': [2,2,3,5,4]},
+               ]
 
 total = []
 
 for clas in classes_list:
-	clas_total = clas.get('scores')
-	total = total + clas_total
+    clas_total = clas.get('scores')
+    total = total + clas_total
 
 scores_qty = len(total)
 scores_sum = sum(total)
 print('The average score in school is', scores_sum / scores_qty)
 
 for clas in classes_list:
-	clas_total = clas.get('scores')
-	class_scores_qty = len(clas_total)
-	class_scores_sum = sum(clas_total)
-	avg_score = class_scores_sum / class_scores_qty
-	print('The average score in the class', clas.get('school_class'), 'is', avg_score)
+    clas_total = clas.get('scores')
+    class_scores_qty = len(clas_total)
+    class_scores_sum = sum(clas_total)
+    avg_score = class_scores_sum / class_scores_qty
+    print('The average score in the class', clas.get('school_class'), 'is', avg_score)
 
 '''
 изящное решение от Саши Телкова
 for clas in classes_list:
-	total += clas['scores']
-	print(total)
+    total += clas['scores']
+    print(total)
 '''
